d1e97c56d2b013f4f71a87a3cea07821eeddda82
Hibot <hui1601@naver.com>
2020-08-30 00:16:26 +0900

    꿈을 깨버린 거시에요

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <stdio.h>
-char arr[] = "adrp.ldrsh.ldnp\0ldpsw\0umax\0stnp.rsubhn\0sqdmlsl\0uqrshl.csel\0umin.usubl.umlsl\0cbnz.adds\0tbnz\0usubl2\0stxr\0sbfx\0strh\0stxrb.adcs\0stxrh\0ands.urhadd\0subs\0sbcs\0fnmadd.ldxrb.saddl\0stur\0ldrsb\0prfm\0ubfiz\0ldrsw.madd.msub.sturb.ldursb\0b.eq\0ldur.sbfiz\0extr\0uqadd\0sshr.uzp1.sttrb\0rsubhn2.ldrh.uqsub\0uqshl\0uabd\0ursra\0usubw\0uaddl2\0b.gt\0b.lt\0sqshl\0bics\0smin.ubfx\0uabdl2\0zip2.ssubw2\0ccmp\0sqdmlal\0b.al\0smax.ldurh.uhsub\0fcvtxn2\0b.pl\0ubfiz.sqdmlsl.tbnz.stnp\0smin\0usubl\0uzp1\0zip2.prfm.strb\0msub\0stxrh.ldxrb\0uqrshl.ldrh\0cbnz\0sshr.ubfx.ldur.ldnp\0adrp.sturb\0smax\0uqsub.sqshlu\0ands\0umin\0ldrsb.ldpsw.rsubhn\0rsubhn2.umlsl2\0sbcs.fmadd\0stur.ldrsh.smlsl2\0ldrsw\0fnmadd\0stxrb.sbfiz\0bics.ldrb\0subs.uhsub\0ldurh";
-int main(){
+#include <string>
+#include <iostream>
+using namespace std;
+string arr1[] = {"adrp.ldrsh.ldnp", "ldpsw", "umax", "stnp.rsubhn", "sqdmlsl", "uqrshl.csel", "sqshlu", "umin.usubl.umlsl", "cbnz.adds", "tbnz", "usubl2", "stxr", "sbfx", "strh", "stxrb.adcs", "stxrh", "ands.urhadd", "subs", "sbcs", "fnmadd.ldxrb.saddl", "stur", "ldrsb", "strb", "prfm", "ubfiz", "ldrsw.madd.msub.sturb.ldursb", "ldrb", "b.eq", "ldur.sbfiz", "extr", "fmadd", "uqadd", "sshr.uzp1.sttrb", "umlsl2", "rsubhn2.ldrh.uqsub", "uqshl", "uabd", "ursra", "usubw", "uaddl2", "b.gt", "b.lt", "sqshl", "bics", "smin.ubfx", "smlsl2", "uabdl2", "zip2.ssubw2", "ccmp", "sqdmlal", "b.al", "smax.ldurh.uhsub", "fcvtxn2", "b.pl"},
+arr2[] = {"saddl", "urhadd", "ubfiz.sqdmlsl.tbnz.stnp", "smin", "strh", "ccmp", "usubl", "umlsl", "uzp1", "sbfx", "b.eq", "zip2.prfm.strb", "msub", "b.pl", "csel", "stxrh.ldxrb", "uqrshl.ldrh", "cbnz", "ursra", "sshr.ubfx.ldur.ldnp", "fcvtxn2", "usubl2", "uaddl2", "b.al", "ssubw2", "umax", "b.lt", "adrp.sturb", "extr", "uqshl", "smax", "uqsub.sqshlu", "ands", "madd", "umin", "b.gt", "uabdl2", "ldrsb.ldpsw.rsubhn", "uqadd", "sttrb", "stxr", "adds", "rsubhn2.umlsl2", "sbcs.fmadd", "usubw", "sqshl", "stur.ldrsh.smlsl2", "ldrsw", "fnmadd", "stxrb.sbfiz", "adcs", "bics.ldrb", "ldursb", "subs.uhsub", "ldurh", "uabd", "sqdmlal"};
+string dream(int param_3){
     int iVar1;
-    size_t sVar2;
-    size_t sVar3;
-    char *__dest;
-    long long uVar4;
-    char *__s;
-    char *__s_00;
-    int param_3 = 0xcad63;
-    int prefix;
-    iVar1 = param_3 + 0x1f;
-    long long cul = (param_3 + (long long) ((param_3 / 0x36 + (param_3 >> 0x1f)) - (int)((long long)param_3 * 0x4bda12f7 >> 0x3f)) * -0x36) * 8;
-    printf("%s.", arr + cul + 6);//IDK why
-    cul = (long long)(iVar1 + ((iVar1 / 0x39 + (iVar1 >> 0x1f)) - (int)((long long)iVar1 * 0x8fb823ef >> 0x3f)) * -0x39) * 8;
-    printf("%s\n", arr + cul + 151);
+    string result;
+    iVar1 = param_3 + 31;
+    long long cul = (long long) (param_3 + ((param_3 / 0x36 + (param_3 >> 0x1f)) - (int)((long long)param_3 * 0x4bda12f7 >> 0x3f)) * -0x36);
+    long long cul2 = (long long)(iVar1 + ((iVar1 / 0x39 + (iVar1 >> 0x1f)) - (int)((long long)iVar1 * 0x8fb823ef >> 0x3f)) * -0x39);
+    result = arr1[cul] + "." + arr2[cul2];
+    return result;
+}
+int main(){
+    for(int i = 1001; i <= 3000; i++){
+        printf("%d : %s\n", i, dream(i).c_str());
+    }
     return 0;
 }
